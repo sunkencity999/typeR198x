510fb95554bdefbb4ee8b78f8db2705392566d0e
Christopher Bradford <christopher.bradford@joby.aero>
2025-12-01 12:16:00 +0900

    Fix: Wrap WebGameView in ContentView to workaround SwiftUI Observation crash on macOS 15.1

--- native/ios/TypeR198XIOS/TypeR198XIOS/TypeR198XIOSApp.swift
+++ native/ios/TypeR198XIOS/TypeR198XIOS/TypeR198XIOSApp.swift
@@ -1,11 +1,19 @@
 import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        WebGameView()
+            .ignoresSafeArea()
+            .statusBar(hidden: true)
+            .persistentSystemOverlays(.hidden)
+    }
+}
+
 @main
 struct TypeR198XIOSApp: App {
     var body: some Scene {
         WindowGroup {
-            WebGameView()
-                .ignoresSafeArea()
+            ContentView()
         }
     }
 }
--- native/macos/typer198xMac/typer198xMac/TypeR198XMacApp.swift
+++ native/macos/typer198xMac/typer198xMac/TypeR198XMacApp.swift
@@ -1,10 +1,16 @@
 import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        WebGameView()
+    }
+}
+
 @main
 struct TypeR198XMacApp: App {
     var body: some Scene {
         WindowGroup {
-            WebGameView()
+            ContentView()
                 .frame(minWidth: 1280, minHeight: 720)
         }
         .commands {
